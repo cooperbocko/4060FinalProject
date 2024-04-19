65b4dc0ef4594890052792a3c5023dee69ceaacc
cooper-bocko-uga <105941591+cooperbocko@users.noreply.github.com>
2024-04-19 12:36:21 -0400

    Login
    got login to work and updated some of the xml ids to be better detailed

--- app/src/main/java/edu/uga/cs/mobliefinalproject/LoginFragment.java
+++ app/src/main/java/edu/uga/cs/mobliefinalproject/LoginFragment.java
@@ -1,36 +1,39 @@
 package edu.uga.cs.mobliefinalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
+import android.util.Patterns;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class LoginFragment extends Fragment {
-
-    private EditText email,password;
-
-    private Button btn;
-
-
-
-
+    private static final String DEBUG = "Login Fragment";
+    private FirebaseAuth mAuth;
+    private EditText et_email;
+    private EditText et_password;
+    private Button btn_login;
 
 
     public LoginFragment() {
         // Required empty public constructor
     }
-
-    // TODO: Rename and change types and number of parameters
     public static LoginFragment newInstance() {
         LoginFragment fragment = new LoginFragment();
         Bundle args = new Bundle();
@@ -43,7 +46,6 @@ public class LoginFragment extends Fragment {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
 
-
         }
     }
 
@@ -54,12 +56,12 @@ public class LoginFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_login, container, false);
 
         //views
-         email = (EditText) rootView.findViewById(R.id.editTextText);
-         password = (EditText) rootView.findViewById(R.id.editTextTextPassword);
-         btn = (Button)  rootView.findViewById(R.id.button3);
+        et_email = rootView.findViewById(R.id.et_email);
+        et_password = rootView.findViewById(R.id.et_password);
+        btn_login = rootView.findViewById(R.id.btn_login);
 
-         //Button listener
-        btn.setOnClickListener(new LoginFragment.ButtonClickListener3());
+        //Button listener
+        btn_login.setOnClickListener(new LoginButtonListener());
 
         //Action Bar Back Button
         MainActivity ap = (MainActivity)getActivity();
@@ -72,21 +74,53 @@ public class LoginFragment extends Fragment {
     }
 
 
-    private class ButtonClickListener3 implements View.OnClickListener {
+    private class LoginButtonListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
+            //get text from edit texts
+            String email = et_email.getText().toString();
+            String password = et_password.getText().toString();
 
-            /*
-            //IF USERNAME AND PASSWORD ARE CORRECT GO TO MAIN FRAGMENT
-            Fragment loginFragment = new LoginFragment();
-            getActivity().getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragmentContainerView3, <add fragment or activity to go to>, null)
-                    .addToBackStack(null)
-                    .commit();
-            */
+            //check email
+            if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                Log.d(DEBUG, "Valid Email");
+            } else {
+                //Toast.makeText(getActivity(), "Invalid Email Address!", Toast.LENGTH_SHORT).show();
+                Log.d(DEBUG, "Invalid Email");
+                return;
+            }
 
+            //check password
+            if (password.isEmpty()) {
+                Toast.makeText(getActivity(), "Password cannot be empty", Toast.LENGTH_SHORT).show();
+                Log.d(DEBUG, "Invalid Password");
+                return;
+            } else {
+                Log.d(DEBUG, "Valid Password");
+            }
 
+            //sign in with built in firebase auth
+            mAuth = FirebaseAuth.getInstance();
 
+            mAuth.signInWithEmailAndPassword( email, password )
+                    .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(DEBUG, "signInWithEmail:success" );
+                                //FirebaseUser user = mAuth.getCurrentUser(); ---> maybe use this for current user
+                                //transition to new fragment and update current user
+
+                            }
+                            else {
+                                // If sign in fails, display a message to the user.
+                                Log.d(DEBUG, "signInWithEmail:failure", task.getException() );
+                                Toast.makeText(getActivity(), "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
         }
     }
 
