65cdc3215623891da479c5d1f8bb0d22c8d61e93
cooper-bocko-uga <105941591+cooperbocko@users.noreply.github.com>
2024-04-21 11:42:04 -0400

    Register
    can now register users

--- app/src/main/java/edu/uga/cs/mobliefinalproject/SignupFragment.java
+++ app/src/main/java/edu/uga/cs/mobliefinalproject/SignupFragment.java
@@ -1,16 +1,27 @@
 package edu.uga.cs.mobliefinalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignupFragment extends Fragment {
+    private static final String DEGUB = "SignupFragment";
 
     private EditText setEmail, setPassword, confirmPassword;
 
@@ -19,9 +30,6 @@ public class SignupFragment extends Fragment {
     public SignupFragment() {
         // Required empty public constructor
     }
-
-
-    // TODO: Rename and change types and number of parameters
     public static SignupFragment newInstance(String param1, String param2) {
         SignupFragment fragment = new SignupFragment();
         Bundle args = new Bundle();
@@ -34,7 +42,6 @@ public class SignupFragment extends Fragment {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
 
-
         }
     }
 
@@ -44,11 +51,57 @@ public class SignupFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView =  inflater.inflate(R.layout.fragment_signup, container, false);
 
-        setEmail = (EditText) rootView.findViewById(R.id.editTextText2);
-        setPassword = (EditText) rootView.findViewById(R.id.editTextText3);
-        confirmPassword = (EditText) rootView.findViewById(R.id.editTextText4);
-        createAcc = (Button) rootView.findViewById(R.id.button5);
+        //views
+        setEmail = rootView.findViewById(R.id.et_registerEmail);
+        setPassword = rootView.findViewById(R.id.et_registerPassword);
+        confirmPassword = rootView.findViewById(R.id.et_confrimPassword);
+        createAcc = rootView.findViewById(R.id.btn_createAccount);
+
+        //set listener
+        createAcc.setOnClickListener(new RegisterButtonClickListener());
+
 
         return rootView;
+    }
+
+    private class RegisterButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            final String email = setEmail.getText().toString();
+            final String password = setPassword.getText().toString();
+
+            final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+            // This is how we can create a new user using an email/password combination.
+            // Note that we also add an onComplete listener, which will be invoked once
+            // a new user has been created by Firebase.  This is how we will know the
+            // new user creation succeeded or failed.
+            // If a new user has been created, Firebase already signs in the new user;
+            // no separate sign in is needed.
+            firebaseAuth.createUserWithEmailAndPassword( email, password )
+                    .addOnCompleteListener( getActivity(), new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+
+                                Toast.makeText( getActivity(),
+                                        "Registered user: " + email,
+                                        Toast.LENGTH_SHORT ).show();
+
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(DEGUB, "createUserWithEmail: success" );
+
+                                //FirebaseUser user = firebaseAuth.getCurrentUser(); ---> maybe use for current user..
+                                //navigate to the home screen
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(DEGUB, "createUserWithEmail: failure", task.getException());
+                                Toast.makeText(getActivity(), "Registration failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
     }
 }
