f99ceb38a969b0c4c134bacf78c76cdd1938ca80
cooper-bocko-uga <105941591+cooperbocko@users.noreply.github.com>
2024-04-27 17:10:09 -0400

    Right fragments
    put get code into right fragments

--- app/src/main/java/edu/uga/cs/mobliefinalproject/OfferFragment.java
+++ app/src/main/java/edu/uga/cs/mobliefinalproject/OfferFragment.java
@@ -2,15 +2,29 @@ package edu.uga.cs.mobliefinalproject;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class OfferFragment extends Fragment {
     private static final String FRAGMENT_POSITION = "position";
+    private static final String DEBUG = "OfferFragment";
+    private List<RideOfferModel> rideOfferModelList;
+    private FirebaseDatabase database;
 
     public OfferFragment() {
         // Required empty public constructor
@@ -32,6 +46,50 @@ public class OfferFragment extends Fragment {
         if (getArguments() != null) {
             int position = getArguments().getInt(FRAGMENT_POSITION);
         }
+
+
+
+        //list of ride offers
+        rideOfferModelList = new ArrayList<>();
+
+
+        //database stuff
+        database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("rideoffers");
+
+        //gets non-accepted and non-owned ride offers
+        myRef.addValueEventListener( new ValueEventListener() {
+
+            @Override
+            public void onDataChange( @NonNull DataSnapshot snapshot ) {
+                // Once we have a DataSnapshot object, we need to iterate over the elements and place them on our job lead list.
+                rideOfferModelList.clear(); // clear the current content; this is inefficient!
+                for( DataSnapshot postSnapshot: snapshot.getChildren() ) {
+                    RideOfferModel rideOfferModel = postSnapshot.getValue(RideOfferModel.class);
+                    rideOfferModel.setKey( postSnapshot.getKey() );
+
+                    //check if accepted or your own offer
+                    if (rideOfferModel.isAccepted() || rideOfferModel.getDriver().equals(CurrentUser.email)) {
+                        Log.d(DEBUG, "Offer not added: " + rideOfferModel);
+                        continue;
+                    } else {
+                        //add offer to list
+                        rideOfferModelList.add( rideOfferModel );
+                        Log.d(DEBUG, "Offer added: " + rideOfferModel);
+
+                    }
+                }
+
+                //implement this later
+                //Log.d( DEBUG_TAG, "ValueEventListener: notifying recyclerAdapter" );
+                //recyclerAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled( @NonNull DatabaseError databaseError ) {
+                Log.d(DEBUG, "Error reading offers from database: " + databaseError);
+            }
+        } );
     }
 
     @Override
--- app/src/main/java/edu/uga/cs/mobliefinalproject/RidesFragment.java
+++ app/src/main/java/edu/uga/cs/mobliefinalproject/RidesFragment.java
@@ -2,15 +2,29 @@ package edu.uga.cs.mobliefinalproject;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RidesFragment extends Fragment {
 
     private static final String FRAGMENT_POSITION = "position";
+    private static final String DEBUG = "RideRequestsFragment";
+    private FirebaseDatabase database;
+    private List<RideRequestModel> rideRequestModelList;
 
 
     public RidesFragment() {
@@ -31,6 +45,49 @@ public class RidesFragment extends Fragment {
         if (getArguments() != null) {
             int position = getArguments().getInt(FRAGMENT_POSITION);
         }
+
+
+        //list of ride offers
+        rideRequestModelList = new ArrayList<>();
+
+
+        //database stuff
+        database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("riderequests");
+
+        //gets non-accepted and non-owned ride requests
+        myRef.addValueEventListener( new ValueEventListener() {
+
+            @Override
+            public void onDataChange( @NonNull DataSnapshot snapshot ) {
+                // Once we have a DataSnapshot object, we need to iterate over the elements and place them on our job lead list.
+                rideRequestModelList.clear(); // clear the current content; this is inefficient!
+                for( DataSnapshot postSnapshot: snapshot.getChildren() ) {
+                    RideRequestModel rideRequestModel = postSnapshot.getValue(RideRequestModel.class);
+                    rideRequestModel.setKey( postSnapshot.getKey() );
+
+                    //check if accepted or your own offer
+                    if (rideRequestModel.isAccepted() || rideRequestModel.getRider().equals(CurrentUser.email)) {
+                        Log.d(DEBUG, "Request not added: " + rideRequestModel);
+                        continue;
+                    } else {
+                        //add request to list
+                        rideRequestModelList.add( rideRequestModel );
+                        Log.d(DEBUG, "Request added: " + rideRequestModel);
+
+                    }
+                }
+
+                //implement this later
+                //Log.d( DEBUG_TAG, "ValueEventListener: notifying recyclerAdapter" );
+                //recyclerAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled( @NonNull DatabaseError databaseError ) {
+                Log.d(DEBUG, "Error reading requests from database: " + databaseError);
+            }
+        } );
     }
 
     @Override
